471811aaf3eceb4efee1e4e33604078a7accaffd
AidanJex101 <115050031+AidanJex101@users.noreply.github.com>
2025-02-24 17:07:38 -0700

    conditional challenges testing

--- chpt 7/conditionalChallenges.js
+++ chpt 7/conditionalChallenges.js
@@ -5,10 +5,16 @@ obj1 = {
   name: "Aidan"
 }
 
+
+
+
+
 obj2 = {
   address: "123 fake st",
   phone: "555-555-5555"
 }
+
+
 
 function combineObjects(obj1, obj2) {
   obj1_Keys = Object.keys(obj1)
@@ -24,7 +30,13 @@ function combineObjects(obj1, obj2) {
   for (let i = 0; i < obj2_Keys.length; i++) {
     obj3[obj2_Keys[i]] = obj2_Values[i]
   }
+
+  return obj3
 }
+
+obj3 = combineObjects(obj1, obj2)
+
+
 
 // Challenge 2
 
@@ -41,17 +53,59 @@ function largestNum(num1, num2) {
 
 function titleCaseString(string) {
   previous_letter = ""
-  for (letter in string) {
-    if (previous_letter = " ") {
-      letter.toUpperCase()
+  newString = ""
+
+  for (let i = 0; i < string.length; i++) {
+    if (previous_letter == " ") {
+      newString += string[i].toUpperCase()
     }
-    previous_letter = letter
+    else {
+      newString += string[i]
+    }
+    
+    previous_letter = string[i]
   }
-  return string
+  return newString
 }
 
 // Challenge 4
 
 function alphabeticalString(string) {
-  
+  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  newString = ""
+  for (let i = 0; i < alphabet.length; i++) {
+    for (let j = 0; j < string.length; j++) {
+      if (alphabet[i] == string[j].toLowerCase()) {
+        newString += string[j]
+      }
+    }
+  }
+  return newString
 }
+
+
+
+// Testing
+
+for (property in obj1) {
+  console.log("\nObject 1\n")
+  console.log(obj1[property])
+}
+
+console.log("\nObject 2\n")
+for (property in obj2) {
+  console.log(obj2[property])
+}
+
+console.log("\nObject 3\n")
+for (property in obj3) {
+  console.log(obj3[property])
+}
+
+console.log("\nFunctions\n")
+
+console.log(largestNum(9,12))
+
+console.log(titleCaseString("I am a sentence"))
+
+console.log(alphabeticalString("Hi there"))
